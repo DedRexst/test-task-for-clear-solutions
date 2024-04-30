283bf1e12139f17fa4c148e5b2b88cb896c9d93a
DedRexst <hlynpavlo@gmail.com>
2024-04-30 09:30:52 +0300

    made some refactoring

--- src/main/java/com/task/config/ApplicationConfig.java
+++ src/main/java/com/task/config/ApplicationConfig.java
@@ -1,13 +1,9 @@
 package com.task.config;
 
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
-@EnableAsync
-@EnableCaching
 @EnableJpaAuditing
 public class ApplicationConfig {
 }
